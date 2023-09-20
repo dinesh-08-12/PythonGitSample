b5864b589c6b8123ddb226fc92491783d267ed48
dinesh-08-12 <dinesh.j.kmu.8@gmail.com>
2023-09-20 09:16:48 +0530

    Structure

---
+++ Test/test_A.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+
+def test_Dinesh(setup):
+    print("Dinesh")
+
+def test_Ram(setup):
+    print("Ram")
+
+def test_Raj(setup):
+    print("Raj")
---
+++ Test/test_B.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+def test_dharani(setup):
+    print("Dharani")
+
+def test_Akash(setup):
+    print("Akash")
+
+def test_Robin(setup):
+    print("Robin")
